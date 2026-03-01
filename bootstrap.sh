2114a915351196fef7a20623f96c2f34d8e955aa
Ax-Projects <101007656+Ax-Projects@users.noreply.github.com>
2026-03-01 13:18:46 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -92,6 +92,7 @@ set -e
 if command -v docker >/dev/null 2>&1; then
     echo "Docker detected! Installing DevPod CLI..."
     curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64" && sudo install -c -m 0755 devpod /usr/local/bin && rm -f devpod
+    devpod provider add docker
 fi
 
 echo "=== Bootstrap Complete! ==="
